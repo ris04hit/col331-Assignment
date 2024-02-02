1c3098c6cf407cb4928ea677d384ad3c3ebdd141
ris04hit <ris04hit@gmail.com>
2024-02-02 17:46:29 +0000

    A1: Done A1 without GUI

--- A1/lab1/mouse.c
+++ A1/lab1/mouse.c
@@ -9,6 +9,8 @@ void
 mousewait_send(void) 
 {
     // Implement your code here
+    int i;
+    for(i = 0; i < 128 && !(inb(MSSTATP) & 0x2); i++);
     return;
 }
 
@@ -17,6 +19,8 @@ void
 mousewait_recv(void) 
 {
     // Implement your code here
+    int i;
+    for (i = 0; i < 32 && (inb(MSSTATP) & 0x1); i++);
     return;
 }
 
@@ -26,6 +30,16 @@ void
 mousecmd(uchar cmd) 
 {
     // Implement your code here
+    mousewait_send();
+    outb(MSSTATP, PS2MS);
+    mousewait_send();
+    outb(MSDATAP, cmd);
+    mousewait_recv();
+    uchar ack;
+    if ((ack = inb(MSDATAP)) != MSACK){
+        cprintf("ACK Message received from mouse: %d\n", ack);
+        panic("Not a valid ack for mouse");
+    }
     return;
 }
 
@@ -33,6 +47,24 @@ void
 mouseinit(void)
 {
     // Implement your code here
+    // Enable Mouse
+    mousewait_send();
+    outb(MSSTATP, MSEN);
+
+    // Send interrupt for mouse state change
+    mousewait_send();
+    outb(MSSTATP, 0x20);
+    mousewait_recv();
+    uchar status_byte = inb(MSDATAP) | 0x02;
+    mousewait_send();
+    outb(MSSTATP, 0x60);
+    mousewait_send();
+    outb(MSDATAP, status_byte);
+
+    mousecmd(0xF6);     // Default Settings
+    mousecmd(0xF4);     // Activate mouse and start sending interrupts
+    ioapicenable(IRQ_MOUSE, 0);     // Receive mouse interrupt on CPU 0
+
     cprintf("Mouse has been initialized\n");    // Do not modify this line
     return;
 }
@@ -40,6 +72,21 @@ mouseinit(void)
 void
 mouseintr(void)
 {
+    uchar packet[3];
+
     // Implement your code here
+    while (inb(MSSTATP) & 0x1){
+        // Receive packet
+        packet[0] = inb(MSDATAP);
+        mousewait_recv();
+        packet[1] = inb(MSDATAP);
+        mousewait_recv();
+        packet[2] = inb(MSDATAP);
+
+        // Process the packet
+        if (packet[0] & 0x1) cprintf("LEFT\n");     // Left Click
+        if (packet[0] & 0x2) cprintf("RIGHT\n");    // Right Click
+        if (packet[0] & 0x4) cprintf("MID\n");      // Middle Click
+    }
     return;
 }
